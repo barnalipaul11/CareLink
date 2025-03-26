f1d04967eb115e0abee9f8fa0bf2106592d308d8
Barnali Paul <barnalipaul890@gmail.com>
2025-03-26 21:25:45 +0530

    second commit

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Index from "./pages/Index";
+import ChatbotPage from "./pages/ChatbotPage";
+import DashboardPage from "./pages/DashboardPage";
+import ChatroomPage from "./pages/ChatroomPage";
+import NotFound from "./pages/NotFound";
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="/chatbot" element={<ChatbotPage />} />
+          <Route path="/dashboard" element={<DashboardPage />} />
+          <Route path="/chatroom" element={<ChatroomPage />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
+
+export default App;
